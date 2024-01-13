46b836c0ae4d27d034c0ec6cd809c750bf0a0c57
Emerald-AM9 <emeraldam9@ms-rss.com>
2024-01-13 15:25:51 +0800

    支持OtherWorldBit

--- globalvars.py
+++ globalvars.py
@@ -26,7 +26,6 @@ try:
     linkbit_not_in_auto_mode = yaml.safe_load(open('linkbit.yaml', 'r', encoding='UTF-8'))
 except FileNotFoundError:
     botfunc.safe_file_write('linkbit.yaml', """# LinkBit配置文件
-owner-qq: 10001
 old-hanbot-api-ip: ""
 old-hanbot-api-key: ""
 current-unix-timestamp: """)
@@ -40,6 +39,7 @@ def linkbit_niam_conf(name: str):
         logger.error(f'{name} 在配置文件中找不到，将尝试空载配置文件')
         return None
 
+# 2kbit-py
 ops = await botfunc.get_all_admin()
 g_ops = await botfunc.get_all_admin()
 blocklist = await botfunc.get_all_sb()
@@ -47,7 +47,7 @@ g_blocklist = await botfunc.get_all_sb()
 ignores = await botfunc.get_all_sb()
 g_ignores = await botfunc.get_all_sb()
 time_now = linkbit_niam_conf('current-unix-timestamp')
-owner_qq = linkbit_niam_conf('owner-qq')
+owner_qq = botfunc.get_config('su')
 api = linkbit_niam_conf('old-hanbot-api-ip')
 api_key = linkbit_niam_conf('old-hanbot-api-key')
 bot_qq = botfunc.get_config('qq')
@@ -56,3 +56,6 @@ database_host = botfunc.get_cloud_config('MySQL_Host')
 database_user = botfunc.get_cloud_config('MySQL_User')
 database_passwd = botfunc.get_cloud_config('MySQL_Pwd')
 database_name = botfunc.get_cloud_config('MySQL_db')
+
+# OtherWorldBit
+botqq = botfunc.get_config('qq')
